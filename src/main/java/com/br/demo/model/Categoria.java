15ce5820df0c141d524ca3d0d689363c8d5ee78c
Wicar Pessoa <wicarpp@gmail.com>
2025-03-08 16:35:06 -0300

    feat: create category model

---
+++ src/main/java/com/br/demo/model/Categoria.java
@@ -0,0 +1,40 @@
+package com.br.demo.model;
+
+public class Categoria {
+    private Long id;
+    private String nome;
+    private String descricao;
+
+    public Categoria(){
+
+    }
+    public Categoria(Long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
